9b1da93ec80422a6de7f972dbd51540853d6ed50
tonyyzhu <tonyyzhu@tpg.com.au>
2014-04-28 02:58:09 +1000

    finally solved issue 26 Add Keywards

--- Lesson_5_How_Programs_Run/26-Adding_Keywords/solutions.py
+++ Lesson_5_How_Programs_Run/26-Adding_Keywords/solutions.py
@@ -5,3 +5,40 @@
 # ALL solutions must be clearly documented
 # ALL solutions must actually work
 # ONLY use concepts covered in the class so far
+
+def hashtable_add(htable,key,value):
+    a = []
+    a = a + [key,value]
+    #print a
+    #b = hash_string(key, len(htable))
+    #print b
+    hashtable_get_bucket(htable,key).append(a)
+
+    return htable
+
+
+def hashtable_get_bucket(htable,keyword):
+    return htable[hash_string(keyword,len(htable))]
+
+def hash_string(keyword,buckets):
+    out = 0
+    for s in keyword:
+        out = (out + ord(s)) % buckets
+    return out
+
+def make_hashtable(nbuckets):
+    table = []
+    for unused in range(0,nbuckets):
+        table.append([])
+    return table
+
+table = make_hashtable(5)
+hashtable_add(table,'Bill', 17)
+hashtable_add(table,'Coach', 4)
+hashtable_add(table,'Ellis', 11)
+hashtable_add(table,'Francis', 13)
+hashtable_add(table,'Louis', 29)
+hashtable_add(table,'Nick', 2)
+hashtable_add(table,'Rochelle', 4)
+hashtable_add(table,'Zoe', 14)
+print table
