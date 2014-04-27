596b2fc25973525e4656dabf456fea1416e12f66
tonyyzhu <tonyyzhu@tpg.com.au>
2014-04-28 00:09:18 +1000

    solution for 23 Empty Hash Table

--- Lesson_5_How_Programs_Run/23-Empty_Hash_Table/solutions.py
+++ Lesson_5_How_Programs_Run/23-Empty_Hash_Table/solutions.py
@@ -5,3 +5,12 @@
 # ALL solutions must be clearly documented
 # ALL solutions must actually work
 # ONLY use concepts covered in the class so far
+
+def make_hashtable(nbuckets):
+    index = []
+    ht = []
+    i = 0
+    while i < nbuckets:
+        ht.append(index)
+        i = i+1
+    return ht
