f788f9fd541e4a14bff66e73e167dcd33433cbe7
tonyyzhu <tonyyzhu@tpg.com.au>
2014-04-28 06:29:49 +1000

    Completed the lesson! But not to problem set yet

--- Lesson_5_How_Programs_Run/31-Population/solutions.py
+++ Lesson_5_How_Programs_Run/31-Population/solutions.py
@@ -5,3 +5,23 @@
 # ALL solutions must be clearly documented
 # ALL solutions must actually work
 # ONLY use concepts covered in the class so far
+
+# Define a Dictionary, population,
+# that provides information
+# on the world's largest cities.
+# The key is the name of a city
+# (a string), and the associated
+# value is its population in
+# millions.
+
+#   Key     |   Value
+# Shanghai  |   17.8
+# Istanbul  |   13.3
+# Karachi   |   13.0
+# Mumbai    |   12.5
+
+Population = {'Istanbul':13.3, 'Karachi':13.0}
+Population['Shanghai'] = 17.8
+Population['Mumbai'] = 12.5
+
+print Population
---
+++ Lesson_5_How_Programs_Run/InstructorsCode.py
@@ -0,0 +1,40 @@
+﻿def get_all_links(page):
+    links = []
+    while True:
+        url, endpos = get_next_target(page)
+        if url:
+            links.append(url) page = page[endpos:]
+        else:
+            break
+    return links
+
+def crawl_web(seed):
+    tocrawl = [seed]
+    crawled = []
+    index = []
+    while tocrawl:
+        page = tocrawl.pop()
+        if page not in crawled:
+            content = get_page(page)
+            add_page_to_index(index, page, content)
+            union(tocrawl, get_all_links(content))
+            crawled.append(page)
+    return index
+
+def add_page_to_index(index, url, content):
+    words = content.split()
+    for word in words:
+        add_to_index(index, word, url)
+
+def add_to_index(index, keyword, url):
+    for entry in index:
+        if entry[0] == keyword: entry[1].append(url)
+            return
+    # not found, add new keyword to index
+    index.append([keyword, [url]])
+
+def lookup(index, keyword):
+    for entry in index:
+        if entry[0] == keyword:
+            return entry[1]
+    return None
