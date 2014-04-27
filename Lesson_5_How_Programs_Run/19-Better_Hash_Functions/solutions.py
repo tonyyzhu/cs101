2628ccd00f714ec838adea95776a043174c7296e
tonyyzhu <tonyyzhu@tpg.com.au>
2014-04-28 02:04:43 +1000

    mod% for loop is not easy to understand

--- Lesson_5_How_Programs_Run/19-Better_Hash_Functions/solutions.py
+++ Lesson_5_How_Programs_Run/19-Better_Hash_Functions/solutions.py
@@ -6,11 +6,41 @@
 # ALL solutions must actually work
 # ONLY use concepts covered in the class so far
 
-def hash_string(keyword,buckets):
+# my original solution
+# def hash_string(keyword,buckets):
     i = 0
     j = 0
     while i<len(keyword):
         j= j + ord(keyword[i])
         i = i + 1
     return (j % buckets)
+
+
+# I don't understand the way this for loop works - it is add sum of modulus together
+# Could it have a possible of that sum > operator?
+# I understand the while loop BTW, which has no amigubity
+
+def hash_string(keyword,buckets):
+    out = 0
+    for s in keyword:
+        print "s", ord(s) % buckets
+        out = (out + ord(s)) % buckets
+        print "Out:", out
+    return out
+
+print "hash_string", hash_string ("Lilith", 5)
+
+def hash_string2(keyword,buckets):
+    i = 0
+    j = 0
+    while i<len(keyword):
+        j= j + ord(keyword[i])
+        i = i + 1
+    return (j % buckets)
+
+print "hash_string2 ", hash_string2 ("Lilith", 5)
+
+print 100 % 6
+
+print ((((((((((((((((20 % 6) + 10) % 6  + 10) % 6 )  + 10) % 6 ) + 10) % 6 )  + 10) % 6 ) + 10) % 6 ) + 10) % 6 ) + 10) % 6 ) % 6
 
