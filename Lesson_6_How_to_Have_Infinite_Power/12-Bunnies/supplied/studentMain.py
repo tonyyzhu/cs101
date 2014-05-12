bd86cbf32ea7c6fbd3f56b77bcb54a6a9111c9f9
tonyyzhu <tonyyzhu@tpg.com.au>
2014-05-12 12:13:08 +1000

    fibonacci

--- Lesson_6_How_to_Have_Infinite_Power/12-Bunnies/supplied/studentMain.py
+++ Lesson_6_How_to_Have_Infinite_Power/12-Bunnies/supplied/studentMain.py
@@ -9,6 +9,13 @@
 #    n > 1 : fibonacci(n) => fibonacci(n-1) + fibonacci(n-2)
 
 def fibonacci(n):
+    if n == 0:
+        return 0
+    else:
+        if n == 1:
+            return 1
+        else:
+            return fibonacci(n-1) + fibonacci(n-2)
 
 
 
