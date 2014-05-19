d8c365dc4e9f28d3972d59c70b76155f045b81e3
tonyyzhu <tonyyzhu@tpg.com.au>
2014-05-19 13:18:19 +1000

    Rabbits

--- Lesson_6_Problem_Set/02-Rabbits_Multiplying/solutions.py
+++ Lesson_6_Problem_Set/02-Rabbits_Multiplying/solutions.py
@@ -5,3 +5,12 @@
 # ALL solutions must be clearly documented
 # ALL solutions must actually work
 # ONLY use concepts covered in the class so far
+
+def rabbits(n):
+    if n == 1:
+        return 1
+    if n == 2:
+        return 1
+    if n <= 5:
+        return (rabbits(n-1) + rabbits(n-2))
+    return (rabbits(n-1) + rabbits(n-2) - rabbits(n-5))
